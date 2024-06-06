a2cf231edc34cfa783a1328ebc2b9dd0582d7c72
Andres Marin PC Main <andres.9125@outlook.com>
2024-06-06 11:48:24 +0200

    Creación de Servidor WEB

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,32 @@
 import store
+from fastapi import FastAPI
+
+#Para que el response sea en HTML
+from fastapi.responses import HTMLResponse
+
+# Instancia de la Aplicación
+app = FastAPI()
+
+# Creación de Primer Recurso
+@app.get('/') # Ruta desde la web, se puede acceder
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact', response_class=HTMLResponse) # Ruta desde la web, se puede acceder
+def get_list():
+    return """
+
+        <html>
+            <head>
+                <h1>Mi primera Web en Python</h1>
+                <h2>Impresionante!</h2>
+            </head>
+            <body>
+                <p>Este Python esta genial!</p>
+            </body>
+        <html>
+
+    """
 
 def run():    
     store.get_categories()
